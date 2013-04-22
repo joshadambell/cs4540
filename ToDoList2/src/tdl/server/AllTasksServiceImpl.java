d5cc126258efc100ea09a73d0c4fe40d34b60e0f
Josh <joshadambell@Joshs-MacBook-Pro-2.local>
2013-04-22 16:06:32 -0600

    stuff

---
+++ ToDoList2/src/tdl/server/AllTasksServiceImpl.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright 2011 Google Inc. All Rights Reserved.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package tdl.server;
+
+import java.util.List;
+
+import tdl.client.AllTasksService;
+import tdl.client.Task;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Transaction;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+public class AllTasksServiceImpl extends RemoteServiceServlet implements AllTasksService {
+	private static final long serialVersionUID = 1L;
+	
+	// Datastore reference
+		private DatastoreService datastore;
+	
+	public AllTasksServiceImpl () {
+		datastore = DatastoreServiceFactory.getDatastoreService();
+	}
+	
+	public List<Task> getAllTasks() {
+		
+		Transaction txn = null;
+		try {
+				txn = datastore.beginTransaction();
+				Entity tasklist = getTaskList();
+				
+				Query query = new Query("Task", datastore.get);
+				Iterable<Entity> allTasks = datastore.prepare(query).asIterable();
+				
+		}
+		finally {
+			if (txn.isActive()) {
+				txn.rollback();
+			}
+		}
+	}
+}
