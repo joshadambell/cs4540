0f142e4a25a31f74c88740329cec8bcd5849c8d6
Josh <joshadambell@Joshs-MacBook-Pro-2.local>
2013-04-22 13:03:58 -0600

    begin

---
+++ todolist/src/tdl/client/Task.java
@@ -0,0 +1,48 @@
+package tdl.client;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Provides objects that encapsulate information about a single task.
+ */
+public class Task implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String key;
+	private String name;
+	private Date date;
+	
+	public Task(String key, String name, Date date) {
+		this.key = key;
+		this.name = name;
+		this.date = date;
+	}
+	
+	public Task () {
+	}
+	
+	public String getKey() {
+		return key;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getDate () {
+		return (date == null) ? "" : date.toString();
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setDate (Date date) {
+		this.date = date;
+	}
+	
+
+}
